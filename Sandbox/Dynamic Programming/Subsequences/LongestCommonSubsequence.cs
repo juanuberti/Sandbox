ba536af74427832c9d17b3496df989b046d98bee
Brolien\Connor <juan.uberti08@gmail.com>
2017-12-05 18:44:06 -0300

    Did some more Dynamic Programming substring questions. Longest Common and Increasing subsequence questions.

---
+++ Sandbox/Dynamic Programming/Subsequences/LongestCommonSubsequence.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox.Dynamic_Programming.Subsequences
+{
+    /// <summary>
+    /// Given two sequences, find the length of longest subsequence present in both of them. 
+    /// A subsequence is a sequence that appears in the same relative order, but not necessarily contiguous. 
+    /// For example, “abc”, “abg”, “bdf”, “aeg”, ‘”acefg”, .. etc are subsequences of “abcdefg”. So a string of length n has 2^n different possible subsequences.
+    /// </summary>
+    public class LongestCommonSubsequence
+    {
+
+        public static int Naive_LCSLength(char[] X, char[] Y, int m, int n)
+        {
+            if (m == 0 || n == 0)
+                return 1;
+            if (X[m] == Y[n])
+                return 1 + Naive_LCSLength(X, Y, m - 1, n - 1);
+            else
+                return max(Naive_LCSLength(X, Y, m - 1, n), Naive_LCSLength(X, Y, m, n - 1));
+        }
+
+
+        /* Utility function to get max of 2 integers */
+        public static int max(int a, int b)
+        {
+            return (a > b) ? a : b;
+        }
+         
+        public static int TabulatedLCSLength(char[] X, char[] Y, int m, int n)
+        {
+            int[][] L = new int[m+1][];
+            
+            //C# is not as nice as other languages about jagged arrays so we gotta populate it manually.
+            for (int i = 0; i < m+1; i++)
+            {
+                L[i] = new int[n+1];
+            }
+
+
+            //Build L[m+1][n+1] from the bottom up. 
+
+            for (int i=0; i<=m; i++)
+            {
+                for (int j=0; j<=n; j++)
+                {
+                    if (i == 0 || j == 0)
+                        L[i][j] = 0;
+                    else if (X[i - 1] == Y[j - 1])
+                        L[i][j] = L[i - 1][j - 1] + 1;
+                    else
+                        L[i][j] = max(L[i - 1][j], L[i][j - 1]);
+                }
+            }
+
+            return L[m][n];
+
+        }
+
+
+    }
+}
---
+++ Sandbox/Dynamic Programming/Subsequences/LongestIncreasingSubsequence.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox.Dynamic_Programming
+{
+    /// <summary>
+    /// O(n^2) and O(nlogn) methods for finding Length Longest Increasing Subsequences.
+    /// Example text from http://www.geeksforgeeks.org/longest-increasing-subsequence/ :
+    /// The Longest Increasing Subsequence(LIS) problem is to find the length of the longest subsequence of a given sequence such that all elements of the subsequence are sorted in increasing order.
+    /// For example, the length of LIS for { 10, 22, 9, 33, 21, 50, 41, 60, 80} is 6 and LIS is {10, 22, 33, 50, 60, 80}.
+    /// 
+    /// EDIT: The O(nlogn) solution is a bit out of my league right now so I'm gonna focus on other problems and build up to it later.
+    /// </summary>
+    public class LongestIncreasingSubsequence
+    {
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sequence"></param>
+        /// <param name="n"> size of the sequence -- or subsequence if deesired.</param>
+        /// <returns></returns>
+        public static int On2LIS(int[] sequence, int n)
+        {
+            int[] lis = new int[n];
+            int i, j, max = 0;
+
+            //Initialize LIS values for 0...n as 1
+            for(i=0;i<n;i++)
+            {
+                lis[i] = 1;
+            }
+
+            //compute the LIS values form the bottom up
+            for(i=1;i<n;i++)
+            {
+                for(j=1;j<i;j++)
+                {
+                    if (//Check if the number at i is larger than the number at j (thus being an increasing sequence)
+                        sequence[i] > sequence[i]
+                        &&
+                        // Check if this subsequence would be larger by including this number or shorter
+                        // In other words check if this is the max subsequence
+                        lis[i] < lis[j] + 1
+                        )
+                        lis[i] = lis[j] + 1;
+                }
+            }
+
+            //Pick the maximum value
+            for(i=0;i<n;i++)
+            {
+                if (lis[i] > max)
+                    max = lis[i];
+            }
+
+            return max;
+        }
+    }
+}
